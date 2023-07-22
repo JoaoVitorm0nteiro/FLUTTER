51058a31783e279d220c2b3e55f8d79fedf504be
JoaoVitorm0nteiro <jv147pao@gmail.com>
2023-07-22 19:29:47 -0300

    add: conclusao algoritmo imc

--- LOGICA_DART/input.dart
+++ LOGICA_DART/input.dart
@@ -1,6 +1,25 @@
-main(){
+import 'dart:io';
 
-stdin.readLineSync();
+main() {
+  print('Digite seu nome:');
+  String? nameString = stdin.readLineSync();
+  print('Prazer $nameString');
 
+  print('Digite seu peso em KG:');
+  final String? pesoString = stdin.readLineSync();
 
+  print('Digite sua altura em metros: ');
+  final String? alturaString = stdin.readLineSync();
+
+  final double pesoKg = double.parse(pesoString!);
+  final double altura = double.parse(alturaString!);
+  double imc = pesoKg / (altura * altura);
+
+  if (imc > 25) {
+    print('Voce esta gordo');
+  } else if (imc < 18) {
+    print('Voce esta magro');
+  } else {
+    print('Voce esta com peso normal.');
+  }
 }
