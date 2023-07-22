0034eb6802c785d2ed1ecef50800277d611b4b81
JoaoVitorm0nteiro <jv147pao@gmail.com>
2023-07-21 22:23:40 -0300

    algoritmo basico imc

---
+++ LOGICA_DART/imc.dart
@@ -0,0 +1,15 @@
+main(){
+// criar imc
+//peso / (altura*altura)
+double peso = 70.3;
+double altura = 1.74;
+
+double resultado = peso/(altura*altura);
+
+print( resultado);
+
+
+}
+// int - inteiro
+// double - decimal
+// String - representa textos
