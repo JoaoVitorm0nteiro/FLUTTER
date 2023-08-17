d63bfc2e0587d5bfa3145cba8c8ff91f57df09d3
JoaoVitorm0nteiro <jv147pao@gmail.com>
2023-08-16 22:05:56 -0300

    algoritmo para exibir numeros pares

---
+++ LOGICA_DART/loopwhile.dart
@@ -0,0 +1,13 @@
+//utilizando a estrutura while
+import 'dart:io';
+
+void main() {
+  String? entrada = stdin.readLineSync();
+  int contador = int.parse(entrada!);
+  while (contador <= contador) {
+    if (contador % 2 == 0) {
+      print(contador);
+    }
+    contador++;
+  }
+}
